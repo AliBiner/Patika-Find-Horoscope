7c9e639c48b58f647865c4a4826de85ce1cecd0a
Ali Biner <alibiner.github@gmail.com>
2025-07-07 10:20:33 +0300

    asks user birthday and birth mount

--- src/main/java/com/alibiner/Main.java
+++ src/main/java/com/alibiner/Main.java
@@ -1,7 +1,16 @@
 package com.alibiner;
 
+import java.util.Date;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Doğum gününüzü giriniz: ");
+        byte birthDay = scanner.nextByte();
+        System.out.print("Doğum ayınızı giriniz: ");
+        byte birthMonth = scanner.nextByte();
+
 
     }
 }
