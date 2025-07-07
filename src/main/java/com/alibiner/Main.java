2e58029571a2aba053786b91315ef756c9305cfd
Ali Biner <alibiner.github@gmail.com>
2025-07-07 10:52:58 +0300

    change horoscope algorithm. switch to if-else

--- src/main/java/com/alibiner/Main.java
+++ src/main/java/com/alibiner/Main.java
@@ -11,80 +11,66 @@ public class Main {
         byte birthMonth = scanner.nextByte();
 
 
-        switch (birthMonth){
-            case 1:
-                if (birthDay<=21)
-                    System.out.println("Oğlak Burcu");
-                else
-                    System.out.println("Kova Burcu");
-                break;
-            case 2:
-                if (birthDay<=19)
-                    System.out.println("Kova Burcu");
-                else
-                    System.out.println("Balık Burcu");
-                break;
-            case 3:
-                if (birthDay<=20)
-                    System.out.println("Balık Burcu");
-                else
-                    System.out.println("Koç Burcu");
-                break;
-            case 4:
-                if (birthDay<=20)
-                    System.out.println("Koç Burcu");
-                else
-                    System.out.println("Boğa Burcu");
-                break;
-            case 5:
-                if (birthDay<=21)
-                    System.out.println("Boğa Burcu");
-                else
-                    System.out.println("İkizler Burcu");
-                break;
-            case 6:
-                if (birthDay<=22)
-                    System.out.println("İkizler Burcu");
-                else
-                    System.out.println("Yengeç Burcu");
-                break;
-            case 7:
-                if (birthDay<=22)
-                    System.out.println("Yengeç Burcu");
-                else
-                    System.out.println("Aslan Burcu");
-                break;
-            case 8:
-                if (birthDay<=22)
-                    System.out.println("Aslan Burcu");
-                else
-                    System.out.println("Başak Burcu");
-                break;
-            case 9:
-                if (birthDay<=22)
-                    System.out.println("Başak Burcu");
-                else
-                    System.out.println("Terazi Burcu");
-                break;
-            case 10:
-                if (birthDay<=22)
-                    System.out.println("Terazi Burcu");
-                else
-                    System.out.println("Akrep Burcu");
-                break;
-            case 11:
-                if (birthDay<=21)
-                    System.out.println("Akrep Burcu");
-                else
-                    System.out.println("Yay Burcu");
-                break;
-            case 12:
-                if (birthDay<=21)
-                    System.out.println("Yay Burcu");
-                else
-                    System.out.println("Oğlak Burcu");
-            default:
-                break;
+        if (birthMonth==1) {
+            if (birthDay<=21)
+                System.out.println("Oğlak Burcu");
+            else
+                System.out.println("Kova Burcu");
+        } else if (birthMonth==2) {
+            if (birthDay<=19)
+                System.out.println("Kova Burcu");
+            else
+                System.out.println("Balık Burcu");
+        } else if (birthMonth==3) {
+            if (birthDay<=20)
+                System.out.println("Balık Burcu");
+            else
+                System.out.println("Koç Burcu");
+        } else if (birthMonth==4) {
+            if (birthDay<=20)
+                System.out.println("Koç Burcu");
+            else
+                System.out.println("Boğa Burcu");
+        } else if (birthMonth==5) {
+            if (birthDay<=21)
+                System.out.println("Boğa Burcu");
+            else
+                System.out.println("İkizler Burcu");
+        } else if (birthMonth==6) {
+            if (birthDay<=22)
+                System.out.println("İkizler Burcu");
+            else
+                System.out.println("Yengeç Burcu");
+        } else if (birthMonth==7) {
+            if (birthDay<=22)
+                System.out.println("Yengeç Burcu");
+            else
+                System.out.println("Aslan Burcu");
+        } else if (birthMonth==8) {
+            if (birthDay<=22)
+                System.out.println("Aslan Burcu");
+            else
+                System.out.println("Başak Burcu");
+        } else if (birthMonth==9) {
+            if (birthDay<=22)
+                System.out.println("Başak Burcu");
+            else
+                System.out.println("Terazi Burcu");
+        } else if (birthMonth==10) {
+            if (birthDay<=22)
+                System.out.println("Terazi Burcu");
+            else
+                System.out.println("Akrep Burcu");
+        } else if (birthMonth==11) {
+            if (birthDay<=21)
+                System.out.println("Akrep Burcu");
+            else
+                System.out.println("Yay Burcu");
+        } else if (birthMonth==12) {
+            if (birthDay<=21)
+                System.out.println("Yay Burcu");
+            else
+                System.out.println("Oğlak Burcu");
         }
 
     }
